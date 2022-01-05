bd898e774aa29ef166739ec917f9a585992530b0
Ashvini4 <80242252+Ashvini4@users.noreply.github.com>
2022-01-05 18:48:05 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,13 @@
+package abstraction_Demo;
+//abstract class
+public abstract class First 
+{
+	//abstract methods
+	abstract public void show();
+	
+	public void display()
+	{
+		//Normal method
+		System.out.println("Abstract Class");
+	}
+}
